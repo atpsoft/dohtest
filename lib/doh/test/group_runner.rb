c4bfde2bea5e1b7c202b09dd6220948bcfe86ede
Makani Mason <gitauthor@pqmland.com>
2012-03-05 10:05:13 -0700

    get all group_runner unit tests passing

--- lib/doh/test/group_runner.rb
+++ lib/doh/test/group_runner.rb
@@ -7,7 +7,7 @@ class GroupRunner
     @group_class,@output = group_class,output
     @config = config || {}
     @group_name = @group_class.to_s
-    @group_failed = false
+    @before_all_failed = false
     @tests_ran = 0
     @tests_skipped = 0
     @assertions_passed = 0
@@ -16,26 +16,28 @@ class GroupRunner
 
   def run
     @output.group_begin(@group_name)
-    create_group
-    run_before_all unless @group_failed
-    run_tests unless @group_failed
-    run_after_all unless @group_failed
+    if create_group
+      run_before_all
+      run_tests unless @before_all_failed
+      run_after_all
+    end
     @output.group_end(@group_name, @tests_ran, @tests_skipped, @assertions_passed, @assertions_failed)
   end
 
   def create_group
     @group = @group_class.new
   rescue => error
-    @group_failed = true
     @output.test_error(@group_name, 'initialize', error)
+    false
   else
     @group.runner = self
+    true
   end
 
   def run_before_all
     @group.before_all if @group.respond_to?(:before_all)
   rescue => error
-    @group_failed = true
+    @before_all_failed = true
     @output.test_error(@group_name, 'before_all', error)
   end
 
@@ -45,26 +47,70 @@ class GroupRunner
     @output.test_error(@group_name, 'after_all', error)
   end
 
+  def find_before_each_method
+    has_before_each = @group.respond_to?(:before_each)
+    has_setup = @group.respond_to?(:setup)
+    if has_before_each && has_setup
+      raise ":before_each and :setup both defined; please pick one"
+    elsif has_before_each
+      @before_each_method = :before_each
+    elsif has_setup
+      @before_each_method = :setup
+    else
+      @before_each_method = nil
+    end
+  end
+
+  def find_after_each_method
+    has_after_each = @group.respond_to?(:after_each)
+    has_teardown = @group.respond_to?(:teardown)
+    if has_after_each && has_teardown
+      raise ":after_each and :teardown both defined; please pick one"
+    elsif has_after_each
+      @after_each_method = :after_each
+    elsif has_teardown
+      @after_each_method = :teardown
+    else
+      @after_each_method = nil
+    end
+  end
+
+  def run_before_each
+    @group.send(@before_each_method)
+  rescue => error
+    @before_each_failed = true
+    @output.test_error(@group_name, @test_name, error)
+  end
+
+  def run_after_each
+    @group.send(@after_each_method)
+  rescue => error
+    @output.test_error(@group_name, @test_name, error)
+  end
+
+  def run_test_method
+    @group.send(@test_name)
+  rescue DohTest::AssertionFailed => failure
+    @assertions_failed += 1
+    @output.assertion_failed(@group_name, @test_name, failure)
+  rescue => error
+    @output.test_error(@group_name, @test_name, error)
+  end
+
   def run_tests
     determine_test_methods
-    has_before_each = @group.respond_to?(:before_each)
-    has_after_each = @group.respond_to?(:after_each)
+    find_before_each_method
+    find_after_each_method
 
     @test_methods.each do |method_name|
       @test_name = method_name
-      begin
-        @group.send(:before_each) if has_before_each
-        @output.test_begin(@group_name, @test_name)
-        @group.send(@test_name)
-        @tests_ran += 1
-        @output.test_end(@group_name, @test_name)
-        @group.send(:after_each) if has_after_each
-      rescue DohTest::AssertionFailed => failure
-        @assertions_failed += 1
-        @output.assertion_failed(@group_name, @test_name, failure)
-      rescue => error
-        @output.test_error(@group_name, @test_name, error)
-      end
+      @before_each_failed = false
+      @output.test_begin(@group_name, @test_name)
+      run_before_each if @before_each_method
+      run_test_method unless @before_each_failed
+      run_after_each if @after_each_method
+      @tests_ran += 1
+      @output.test_end(@group_name, @test_name)
     end
   end
 
