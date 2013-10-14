72c1edb8eaacd66199889ded610f19bf04af1b0e
Makani Mason <gitauthor@pqmland.com>
2013-10-14 13:30:25 -0600

    return true if any files were required

--- lib/dohtest/require_paths.rb
+++ lib/dohtest/require_paths.rb
@@ -2,13 +2,18 @@ module DohTest
 extend self
 
 def require_paths(glob, paths)
+  retval = false
   paths.collect {|elem| File.expand_path(elem) }.each do |onepath|
     if File.directory?(onepath)
-      Dir.glob(File.join(onepath, '**', glob)).each {|filename| require(filename)}
+      Dir.glob(File.join(onepath, '**', glob)).each do |filename|
+        retval = true
+        require(filename)
+      end
     else
       require(File.expand_path(onepath))
     end
   end
+  retval
 end
 
 end
