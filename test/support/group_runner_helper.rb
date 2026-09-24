046e2106a37ecc939211fb2b2241439a11a5cc0a
Makani Mason <714199+makanimason@users.noreply.github.com>
2026-09-24 13:31:08 -0700

    refactor out GroupRunnerHelper for reuse

---
+++ test/support/group_runner_helper.rb
@@ -0,0 +1,26 @@
+require 'dohtest/group_runner'
+require 'dohtest/capture_output'
+
+module DohTest
+
+# runs a test group with CaptureOutput, so a Minitest::Test can check the events it reports
+module GroupRunnerHelper
+  def verify_event(expected_pairs, event)
+    expected_pairs.each_pair do |key, value|
+      assert_equal(value, event[key])
+    end
+  end
+
+  def run_group(group_klass, grepstr = nil)
+    @group_klass = group_klass
+    @output = CaptureOutput.new
+    config = if grepstr then DohTest.config.merge({:grep => grepstr}) else nil end
+    @runner = GroupRunner.new(@group_klass, @output, config)
+    @runner.run
+    @events = @output.events
+    assert_equal({:name => 'group_begin', :group_name => @group_klass.to_s}, @events.shift)
+    verify_event({:name => 'group_end', :group_name => @group_klass.to_s}, @events.last)
+  end
+end
+
+end
